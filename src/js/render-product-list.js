9de4a08fdc5a49fe99787300e4c4387ca749c457
Afiron09 <comixgod@gmail.com>
2023-11-30 20:23:27 +0200

    1

--- src/js/render-product-list.js
+++
@@ -1,5 +0,0 @@
-import { markupProductsList } from './markup-product-list';
-
-export function renderProductList(elemRef, products) {
-  elemRef.innerHTML = markupProductsList(products);
-}
--- src/main.js
+++ src/main.js
@@ -1,12 +1,1 @@
-import './js/home';
-import './js/test-api';
-import './js/aside';
-import './js/modal-markup';
-import './js/select';
-import './js/modal-success';
-import './js/product-list-click';
-import './js/subscription';
-import './js/pagination/pagination-handler';
-import './js/scroll-up';
-import './js/filter';
-// import './js/markup-product-list';
+import "./js/home";
